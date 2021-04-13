5155112f9c729a70221bb82aae7c9f5332602790
Moshood Sikiru <sikirumoshood@gmail.com>
2021-04-13 09:57:27 +0100

    fix: reduce max db connection to 10

--- src/app/database/index.ts
+++ src/app/database/index.ts
@@ -6,7 +6,12 @@ const promise = require('bluebird');
 
 const pg = pgp({ promiseLib: promise, noLocking: true });
 
-//@ts-ignore
-const inventoryDB = pg(config.DATABASE_URL);
+const connectionOptions = {
+    //@ts-ignore
+    connectionString: config.DATABASE_URL,
+    max: 10
+};
+
+const inventoryDB = pg(connectionOptions);
 
 export default inventoryDB;
