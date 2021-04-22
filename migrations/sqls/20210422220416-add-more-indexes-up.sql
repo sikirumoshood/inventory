cd550250c0598b6b849102428035909531217d67
Moshood Sikiru <sikirumoshood@gmail.com>
2021-04-23 00:45:53 +0100

    Fix migration

--- migrations/sqls/20210422220416-add-more-indexes-up.sql
+++ migrations/sqls/20210422220416-add-more-indexes-up.sql
@@ -1,5 +1,5 @@
 /* Replace with your SQL commands */
-CREATE INDEX item_id_index ON item_types(id);
-CREATE INDEX inventory_id_index ON inventories(id);
-CREATE INDEX inventory_item_id_index ON inventories(item_id);
-CREATE INDEX quantity_index ON inventories(quantity);
+CREATE INDEX IF NOT EXISTS item_id_index ON item_types(id);
+CREATE INDEX IF NOT EXISTS inventory_id_index ON inventories(id);
+CREATE INDEX IF NOT EXISTS inventory_item_id_index ON inventories(item_id);
+CREATE INDEX IF NOT EXISTS quantity_index ON inventories(quantity);
